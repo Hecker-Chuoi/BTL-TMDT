1da143b706bea55dfe52a2fcbacc5a8a3637ee03
Hecker Chuoi <nguyentienht1910z@gmail.com>
2026-03-05 08:04:47 +0700

    init

---
+++ Backend/src/main/java/ptit/btl/httmdt/BtlHttmdtApplication.java
@@ -0,0 +1,13 @@
+package ptit.btl.httmdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlHttmdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlHttmdtApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ptit/btl/httmdt/BtlHttmdtApplicationTests.java
@@ -0,0 +1,13 @@
+package ptit.btl.httmdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlHttmdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
